b9c00b0faa0d12fad39f92696dd351250ffe3b22
bnbsking <bnbsking@gmail.com>
2025-12-04 15:33:41 +0800

    add

--- Database/relational_db_postgre/run.sh
+++ Database/relational_db_postgre/run.sh
@@ -5,3 +5,5 @@ docker-compose up -d
 # docker-compose down # to stop and remove containers
 
 # docker-compose stop # to stop containers without removing them
+
+# use dbeaver to visualize and manage the database
